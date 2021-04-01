0523a552c13cf94da56fa33c291a1983ad8139fa
Joshua Retallick <josh@old-empire.co.uk>
2021-04-01 12:23:54 +0100

    T1

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,42 +1,13 @@
-def blackjack
+class Card
 
-    card_values = {
-    "Ace"=>11, "2"=>2, "3"=>3, "4"=>4, "5"=>5,
-    "6"=>6, "7"=>7, "8"=>8, "9"=>9, "10"=>10,
-    "Jack"=>10, "Queen"=>10, "King"=>10
-    }
+  attr_reader :suit
 
-    cards = ["Ace", "2", "3", "4", "5", "6", "7",
-          "8", "9", "10", "Jack", "Queen", "King"]
+  def initialize
+    @suit = :clubs
+  end
 
-    sam_total = 0
-    dealer_total = 0
-
-    sam_hand = []
-    dealer_hand = []
-
-    2.times { sam_hand << cards.sample }
-    2.times { dealer_hand << cards.sample }
-
-    sam_hand.each do |score|
-      sam_total += card_values[score]
-    end
-    puts "Sams Hand: #{sam_hand}, Sams Total: #{sam_total}"
-
-    dealer_hand.each do |score|
-      dealer_total += card_values[score]
-    end
-    puts "Dealers Hand: #{dealer_hand}, Dealers Total: #{dealer_total}"
-
-    if sam_total == 21 && dealer_total == 21
-      puts "Its a draw!"
-    elsif sam_total == 21
-      puts "Sam got Blackjack!"
-    elsif dealer_total == 21
-      puts "You lose! Dealer got Blackjack!"
-    else
-      sam_hand << cards.sample
-      puts "Sams Hand: #{sam_hand}, Sams Total: #{sam_total}"
-    end
+  def suit
+    @suit
+  end
 
 end
--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -1,1 +1,14 @@
 require 'blackjack'
+
+describe Card do
+
+  subject(:card) { described_class.new }
+
+  it "should have a suit" do
+    expect(card.suit).not_to eq nil
+  end
+
+  it "should have a value"
+  it "should accept suit and value when building"
+
+end
